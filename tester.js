173e8799bfb20e54c693f167ebc6c12d95298702
unibreakfast <unibreakfast1@gmail.com>
2019-12-26 08:54:21 +0200

    tester prettier

--- tester.js
+++ tester.js
@@ -3,11 +3,12 @@ JSON.same =(a, b)=> JSON.stringify(a) == JSON.stringify(b)
 const
   c = console.log,
   sleep = time => new Promise(wakeUp => setTimeout(wakeUp, time)),
-  test = (title, finish, err) => [
-    msg => !err && c("TEST: "+title) || c("FAIL: "+msg) || (err=1),
-    ()=> !err && (c("TEST: "+title) || c("OK: "+finish)),
-    msg => {!err && c("TEST: "+title) || c("FAIL: "+msg) || (err=1); throw msg}
-  ],
+  test = (title, finish, err) => (title = "-".repeat(80)+"\nTEST: "+title) &&
+    [
+      msg => !err && c(title) || c("FAIL: "+msg) || (err=1),
+      ()=> !err && (c(title) || c(" OK:  "+finish)),
+      msg => {!err && c(title) || c("FAIL: "+msg) || (err=1); throw msg}
+    ],
   makeTest =(title, ok, checkFn)=> async swallow => {
     const [ fail, end, crit ] = test(title, ok)
     await checkFn(fail, crit, sleep)
