5554f6d8860450927f0adb6f6f6978b9369222b1
unibreakfast <unibreakfast1@gmail.com>
2019-12-26 15:13:45 +0200

    delays const redefined

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const
 
   reg = async (login, pass) => await dataElf.addUser(login, await hash(pass,4)),
 
+  DELAYS = [0, 0, 0, 3, 7, 15, 30, 60, 120, 240 /* seconds */].map(n => 1e3*n),
+  RESET_TIME = 3e5, //5mins
   idLock = {}, loginLock = {},
-  secs = [0, 0, 0, 3, 7, 15, 30, 60, 120, 240],
 
   check = async (ref, pass) => {
     if (idLock[ref] || loginLock[ref]) return null
@@ -17,8 +18,8 @@ const
     if (!user) return false;
     (idRef? idLock : loginLock)[ref] = true
     const now = Date.now(), { last=now, guess=-1, hash, id } = user,
-          nowGuess = now-last > 3e5? 0 : min(guess+1, 9),
-          delay = max(0, secs[nowGuess]*1e3 - (now-last));
+          nowGuess = now-last > RESET_TIME? 0 : min(guess+1, DELAYS.length-1),
+          delay = max(0, DELAYS[nowGuess] - (now-last));
     dataElf.updUser(id, {guess: nowGuess})
     await new Promise(go => setTimeout(go, delay))
     const verdict = await compare(pass, hash)
